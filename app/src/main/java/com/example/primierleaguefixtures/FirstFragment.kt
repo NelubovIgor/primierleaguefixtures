f71974cc5929d563410bf3fd6da7b5d77485a75d
nelubov <nelubov.igor@gmail.com>
2024-06-23 18:25:23 +0300

    task2

---
+++ app/src/main/java/com/example/primierleaguefixtures/FirstFragment.kt
@@ -0,0 +1,30 @@
+package com.example.primierleaguefixtures
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+
+class FirstFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val buttonfrag = view.findViewById<Button>(R.id.frag2)
+        val navController = findNavController()
+//        navController.navigate(R.id.action_fragmentOne_to_fragmentTwo)
+        buttonfrag.setOnClickListener { navController.navigate(R.id.secondFragment) }
+    }
+
+
+}
